81a16abe9f019f8ac77c4d25020ca1158e11e26b
Daniel Olagunju <todak2000@gmail.com>
2018-12-17 14:37:45 +0100

    erewsds

--- profile.php
+++ profile.php
@@ -46,10 +46,12 @@
                 // $Image = $row['image'];
             }
          // diet update messages QUERY
-         // $querya = "SELECT * FROM `diet_update` WHERE email = '$Email' AND seen = '1' order by id desc";
-         // $resulta = mysqli_query($con,$querya) or die(mysqli_error());
-         //  $rowsa = mysqli_num_rows($resulta);
-       
+         $querya = "SELECT * FROM `diet_update` WHERE email = '$Email' AND seen = '1' order by id desc";
+         $resulta = mysqli_query($con,$querya) or die(mysqli_error());
+          $rowsa = mysqli_num_rows($resulta);
+          if ($rowsa){
+
+
             ?>
       <header class="bar bar-nav">
       <h1 class="title">Profile</h1>
@@ -152,11 +154,11 @@
       </a>
       <a class="tab-item" name="dieting"  href="diet_update_history.php" style="text-decoration: transparent;">
       <span class="span" id="notification" style="background-color:red;width:20px; margin-left:auto; margin-right:auto; border-radius:50%;">
-      <!-- 	<?php 
+      	<?php 
+      echo $rowsa;
       
-      if (!$rowsa){
-        echo "12";
-      }else{echo $rowsa;} ?> -->
+        
+      }else{echo "12";} ?> 
       </span> 
       <i class="fa fa-gift" id="diet"></i>
         <span class="span">Diet Update</span> 
